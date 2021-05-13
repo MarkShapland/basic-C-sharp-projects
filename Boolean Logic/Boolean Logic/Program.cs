91d25b5ec3291de48b2b36f3eb01b89082c15592
Mark Shapland <mark_shapland@hotmail.com>
2021-05-13 12:25:02 -0700

    Boolean Logic insurance

---
+++ Boolean Logic/Boolean Logic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false|| false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(false != true);
+
+            // xor operator
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedTickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualifies = (age > 15 && !dui && speedTickets < 4);
+            Console.WriteLine("Qualified: " + qualifies);
+            Console.ReadLine();
+        }
+    }
+}
+
