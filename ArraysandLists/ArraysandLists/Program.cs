c90b4bec4845d4c081bd9bf6469575eef1ff8d10
Mark Shapland <mark_shapland@hotmail.com>
2021-05-14 15:30:52 -0700

    CONSOLE APP ARRAY SUBMISSION ASSIGNMENT

---
+++ ArraysandLists/ArraysandLists/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArraysandLists
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //lists integer
+            List<int> intlist = new List<int>();
+            intlist.Add(4);
+            intlist.Add(10);
+            intlist.Remove(10);
+            intlist.Add(5);
+            //string list
+            List<string> strList = new List<string>();
+            strList.Add("mark");
+
+            
+            Console.WriteLine(intlist[0]);
+            Console.ReadLine();
+        //{// Both of these create the same array
+        // use array when you have a fixed quantity, storing large quantity of something, 
+        //    int[] numArray = new int[5];
+        //    numArray[0] = 5;
+        //    numArray[1] = 2;
+        //    numArray[2] = 10;
+        //    numArray[3] = 200;
+        //    numArray[4] = 5000;
+
+        //    int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+        //    int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+        //    // how to change index in array
+        //    numArray2[5] = 650;
+        //    Console.WriteLine(numArray2[5]);
+        //    Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ stringFunctions/stringFunctions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jesse";
+            //string quote = "The main said, \"Hello\", Jesse. \n \t Hello on a tab";
+            ////  escape character \
+            //// escape sequence inside stringfor entire string @ everything in between "". example @"c:/users/filename";
+
+            //bool trueOrFalse = name.Contains("s"); //true
+            //trueOrFalse = name.EndsWith("s"); //false
+            //int length = name.Length; //length of name
+            //name.ToUpper(); // converts to uppercase
+            //name.ToLower(); // converts to lower
+            //// strings are immutable = once created strings can't be changed.
+
+            //StringBuilder sb = new StringBuilder();// string builder, dynamic object throw string into it and expand
+            //sb.Append("my name is Jesse;");
+            //Console.WriteLine(quote);
+            //Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+
+            ;
+            string hello = "Hello, ";
+
+            
+            string name = "My name is";
+
+            
+            string chris = "Chris";
+
+            string fullName = string.Concat(hello, name, chris);
+            string paragraph = ("Hello, \n my name \n is Chris.");
+
+            chris.ToUpper();
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+
+        }
+    }
+}
