5fc0f36eef1744b6bcef2300d3ffa637cf6513cb
Mark Shapland <mark_shapland@hotmail.com>
2021-05-14 08:20:35 -0700

    BRANCHING SUBMISSION ASSIGNMENT

---
+++ Branching_Submission_Assignment/Branching_Submission_Assignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Branching_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50) 
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            } 
+            else 
+            {
+                Console.WriteLine("Please enter the dimensions of the package.");
+                
+            }
+
+            Console.WriteLine("width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int dimensions = width + height + length;
+            int product = width * height * length;
+
+            if (dimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            else
+            {
+                int total = product * weight / 100;
+                Console.WriteLine("Your estimated total for shipping this package is: $" + total + " Thank you!");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Tenerary operator challenge/Tenerary operator challenge/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Tenerary_operator_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 = 7;
+            //int num2 = 16;
+
+            //string result = num1 > num2 ? "num1 is greater than num2." : "num2 is greater than num1.";
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+            //Ternary operator
+            //int currentTemperature = 80;
+
+            //int roomTemperature = 70;
+
+            //string comparisonResults = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp.";
+            //Console.WriteLine(comparisonResults);
+            //Console.ReadLine();
+            //Branching
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("Room temperature is warmer than current temperature.");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature");
+
+            //    int roomTemperature = 70;
+
+            //    Console.WriteLine("Hi, What is your name?");
+            //    string name = Console.ReadLine();
+
+            //    Console.WriteLine("Hi, " + name + "what is the temperature where you are?");
+            //    int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //           if (currentTemperature == roomTemperature)
+            //        {
+            //        Console.WriteLine("It is exactly room temperature.");
+            //    }
+            //           else if (currentTemperature > roomTemperature)
+            //    {
+            //        Console.WriteLine("It is warmer than room temperature.");
+            //    }
+            //           else if ( roomTemperature > currentTemperature)
+            //    {
+            //        Console.WriteLine("It is colder than room temperature.");
+            //    }
+
+            //           else
+            //    {
+            //        Console.WriteLine("Uhhhh....something went wrong.");
+            //    }
+            //    Console.ReadLine();
+            //ternary operator
+            Console.WriteLine("What is your favorite number");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
