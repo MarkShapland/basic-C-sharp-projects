433a6fb0dc61b5e5e64a8cfd1dd6af4dc9f314ca
Mark Shapland <mark_shapland@hotmail.com>
2021-05-18 10:23:25 -0700

    Exception assignment

---
+++ STRINGS AND INTEGERS SUBMISSION ASSIGNMENT/STRINGS AND INTEGERS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace STRINGS_AND_INTEGERS_SUBMISSION_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Take user input and divide each number in list
+            try
+            {
+                List<int> num1 = new List<int>();
+                num1.Add(2);
+                num1.Add(4);
+                num1.Add(6);
+                num1.Add(8);
+                Console.WriteLine("Enter a number to be divided by in the list.");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();
+
+                foreach (int num in num1)
+                {
+                    Console.WriteLine(num + " divided by " + num2 + " equals " + num / num2);
+                    Console.ReadLine();
+                }
+                //
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please divide by zero.");
+
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number.");
+
+            }
+            finally
+            {
+                Console.ReadLine();
+                Console.WriteLine(" Block is Executed");
+                Console.ReadLine();
+            }
+
+        }    
+    }
+}
