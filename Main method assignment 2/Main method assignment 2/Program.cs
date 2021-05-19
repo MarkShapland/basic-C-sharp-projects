daac94f61fb3ae518e721b6312d05333fc1d763d
Mark Shapland <mark_shapland@hotmail.com>
2021-05-19 08:38:15 -0700

    Method assignment 2

---
+++ Main method assignment 2/Main method assignment 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_method_assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            math mathNumbers = new math();
+            Console.WriteLine("Input two numbers one at a time. You do not need to enter a second number.");
+            Console.WriteLine("Input first number; ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input second number: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(mathNumbers.AddNumber(input1, input2));
+            Console.ReadLine();
+            
+
+            
+
+
+
+            
+
+
+
+        }
+    }
+}
---
+++ Main method assignment 2/Main method assignment 2/math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_method_assignment_2
+{
+    class math
+    {
+        public int  AddNumber(int number1,  int number2 )
+        {
+            int total = number1 + number2;
+            return total;
+        }
+
+    }
+}
