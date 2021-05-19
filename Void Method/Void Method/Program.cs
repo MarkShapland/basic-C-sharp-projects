adf016c778245072ccce9b0ab0036818b331bf1d
Mark Shapland <mark_shapland@hotmail.com>
2021-05-19 14:17:11 -0700

    Void method assignment

---
+++ Void Method/Void Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Program
+    {
+        static void Main(string[] args)//instantiate the class
+        {
+            Void_Method void1 = new Void_Method();
+
+            void1.voidAssignment(12, 15);//passing in two integers
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Void Method/Void Method/Void_Method.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Void_Method
+    {
+        public void voidAssignment(int number1, int number2)
+        {
+            int total = number1 + 25; // math operation
+            Console.WriteLine("Here is the second integer: " + number2); // outputs only the second integer  
+            
+        }
+    }   
+}
