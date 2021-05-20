367bfc8e0631ba11fb6b111743eb1e9563499161
Mark Shapland <mark_shapland@hotmail.com>
2021-05-20 12:01:01 -0700

    Abstract class assignment

---
+++ Abstract class  asignment/Abstract class  asignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class__asignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Name1 = new Employee();
+            Name1.FirstName = "Sample";
+            Name1.LastName = "Student";
+            Name1.SayName();
+            Console.ReadLine();
+
+
+        }
+    }
+}
