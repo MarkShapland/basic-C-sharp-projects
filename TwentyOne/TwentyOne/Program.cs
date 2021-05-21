2996b2c098a2b5e5603af5eb12506f5f5440f44a
Mark Shapland <mark_shapland@hotmail.com>
2021-05-21 14:56:08 -0700

    lambda expressions

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -9,18 +9,11 @@ namespace TwentyOne
     class Program
     {
         static void Main(string[] args)// string array 
+
         {
-           
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Face = Face.Eight;
-            card2.Face = Face.King;
-
-            Console.WriteLine(card1.Face);
-
             //Deck deck = new Deck();
-            //deck.Shuffle(3);
 
+            //deck.Shuffle(3);
 
             //foreach (Card card in deck.Cards)
             //{
@@ -28,9 +21,39 @@ namespace TwentyOne
             //}
             //Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
+
+
+
+
+            ////List<Card> newlist = deck.Cards.Where(x => x.Face == Face.King).ToList();
+
+            ////int count = deck.Cards.Count(x => x.Face == Face.Ace);//cards is a list and Count is a Lamda function
+
+            //List<int> numberlist = new List<int>() { 1, 2, 3, 535, 45, 90 };
+            //int sum = numberlist.Where(x => x > 20).Sum(); 
+            //Console.WriteLine(sum);
+
+
+            //int counter = 0; //counts all the Ace Face card in deck
+            //foreach (Card card in deck.Cards)
+            //{
+            //    if (card.Face == Face.Ace) //nested 
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //foreach (Card card in newlist)
+            //{
+            //    Console.WriteLine(card.Face);
+            //}
+
+
+
+
+
         }
-        
-        
+
+
 
         //Game game = new TwentyOneGame(); //polymorphism
         //game.Players = new List<Player>(); //instantiated list  
@@ -54,7 +77,11 @@ namespace TwentyOne
         //    return deck;
 
         //}
-
+        //Card card1 = new Card();
+        //Card card2 = card1;
+        //card1.Face = Face.Eight;
+        //card2.Face = Face.King;
+        //Console.WriteLine(card1.Face);
 
     }
 }
