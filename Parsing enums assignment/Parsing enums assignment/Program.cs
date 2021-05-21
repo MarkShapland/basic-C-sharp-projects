44170b62079ea4187e92eca69919aa3b37b9605b
Mark Shapland <mark_shapland@hotmail.com>
2021-05-20 22:14:03 -0700

    enums assignment

--- Parsing enums assignment/Parsing enums assignment/Program.cs
+++ Parsing enums assignment/Parsing enums assignment/Program.cs
@@ -8,8 +8,46 @@ namespace Parsing_enums_assignment
 {
     class Program
     {
+        public enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+
         static void Main(string[] args)
         {
-        }
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week: ");
+                string CurrentDay = Console.ReadLine().ToLower();
+                Days day;
+
+                if (Enum.TryParse<Days>(CurrentDay, out day))
+                {
+                    Console.WriteLine("Good job on knowing the day is " + day);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    throw new OverflowException();
+                }
+            }
+            catch (OverflowException)
+
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();    
+        }   
+            
+        
+
+     
     }
 }
