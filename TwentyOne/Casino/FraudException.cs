0ef26c0cebb7ab0e4f7a8f30de48c5d7eb359233
Mark Shapland <mark_shapland@hotmail.com>
2021-05-26 12:40:11 -0700

    exceptions

---
+++ TwentyOne/Casino/FraudException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class FraudException : Exception //inheriting from exception, all specific exceptions will inherit from exceptions: a form of polymorphism.
+    {
+        public FraudException() //standard constructor
+            : base() {} //inheriting from base Exception
+        public FraudException(string message) //overloading constructor method
+        : base(message) { } 
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -17,8 +17,18 @@ namespace TwentyOne
 
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
-            Console.WriteLine("How much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");// if the user inputs a string
+                validAnswer = int.TryParse(Console.ReadLine(), out bank); //similar to convert 
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+
+
+            
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName); //players name will be place in placeholder {0}
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
@@ -35,7 +45,23 @@ namespace TwentyOne
                 player.isActivelyPlaying = true; //creates new GUID for each player so we can track player
                 while (player.isActivelyPlaying && player.Balance >0) // keeps player in the while loop
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+
+                    {
+                        Console.WriteLine("An error occured please contact your system administrator.");
+                        Console.ReadLine();
+                        return; 
+                    }
                 }
                 game -= player; //exits player from loop
                 Console.WriteLine("Thank you for playing!");
